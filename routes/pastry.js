386384291c0d46bd78fd44edac41169db6b5d792
jyothsna9797 <pamarthi.jyothsna97@gmail.com>
2022-11-01 18:42:52 -0500

    updated captital letter in the class name

--- routes/pastry.js
+++ routes/pastry.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pastry', { title: 'Search Results pastry' });
+  res.render('pastry', { title: 'Search Results Pastry' });
 });
 
 module.exports = router;
