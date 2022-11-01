26ffb3b994d446a01a2df20c3bbef73fe9b0744f
jyothsna9797 <pamarthi.jyothsna97@gmail.com>
2022-11-01 18:29:48 -0500

    removed unnecessary files from folder

--- routes/vehicle.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('vehicle', { title: 'Search Results vehicle' });
-});
-
-module.exports = router;
